35bc866a1546e36a72a7ecf036b6377443f5c2ac
EnechukwuChibuike <enechukwuchibuike@gmail.com>
2021-06-18 22:49:09 +0100

    first commit

---
+++ dist/assets/app.js
@@ -0,0 +1,29 @@
+function search(event) {
+   let inputSearch = document.getElementById("searchBook").value;
+   const url = `https://www.googleapis.com/books/v1/volumes?q=${inputSearch}`;
+   event.preventDefault();
+   fetch(url)
+      .then((resp) => resp.json())
+      .then(function (data) {
+         let row = document.getElementById("row");
+         let books = data.items;
+         books.map((item) => {
+            let col = document.createElement("div");
+            col.classList.add("col-md-3");
+            let des = `
+               <div>
+                  <img src="${item.volumeInfo.imageLinks.thumbnail}">
+               </div>
+               <div>
+                  <h6> ${item.volumeInfo.title}</h6>
+               </div>
+            `;
+            col.innerHTML = des;
+            row.appendChild(col);
+            console.log(item.volumeInfo.title);
+         });
+         console.log(books);
+      });
+   document.getElementById("searchBook").value = "";
+   updateSearch();
+}
