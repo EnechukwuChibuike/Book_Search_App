0b077ed9ffe979d3822f7d6d5ca2a4d300b7ed9e
EnechukwuChibuike <enechukwuchibuike@gmail.com>
2021-06-20 23:39:50 +0100

    added changes

--- dist/assets/app.js
+++ dist/assets/app.js
@@ -1,4 +1,5 @@
 function search(event) {
+   document.getElementById("row").innerHTML = "";
    let inputSearch = document.getElementById("searchBook").value;
    const url = `https://www.googleapis.com/books/v1/volumes?q=${inputSearch}`;
    event.preventDefault();
@@ -9,21 +10,23 @@ function search(event) {
          let books = data.items;
          books.map((item) => {
             let col = document.createElement("div");
-            col.classList.add("col-md-3");
             let des = `
                <div>
                   <img src="${item.volumeInfo.imageLinks.thumbnail}">
                </div>
                <div>
                   <h6> ${item.volumeInfo.title}</h6>
+                  <p><a target="blank" href="${item.volumeInfo.infoLink}">Know more</a></p>
                </div>
             `;
             col.innerHTML = des;
             row.appendChild(col);
-            console.log(item.volumeInfo.title);
+            console.log(item.volumeInfo);
          });
          console.log(books);
+      })
+      .catch((error) => {
+         alert("invalid information");
       });
    document.getElementById("searchBook").value = "";
-   updateSearch();
 }
